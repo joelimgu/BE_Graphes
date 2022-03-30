70826f5531990587ece9093a567a0d0d2a4d85b8
Aude Jean-Baptiste <jeaaud1@alum.us.es>
2022-03-30 13:40:02 +0200

    fin class labelList, rien testé

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,7 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Graph;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,6 +12,9 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
+        Graph graph = data.getGraph();
+        // peut-être plus simple de faire la liste ici plutôt que dans une classe à part?
+        LabelList labels = new LabelList(graph);
         // TODO:
         return solution;
     }
