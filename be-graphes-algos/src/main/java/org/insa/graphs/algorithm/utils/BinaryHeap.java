8bfd7d5a2a74f663a9804585647cc295a8f668ae
Aude510 <dunae97300@outlook.fr>
2022-05-20 09:00:59 +0200

    implemented hashmap to reduce complexity of dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/utils/BinaryHeap.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/utils/BinaryHeap.java
@@ -1,5 +1,6 @@
 package org.insa.graphs.algorithm.utils;
 
+import java.util.HashMap;
 import java.util.ArrayList;
 
 /**
@@ -17,7 +18,9 @@ public class BinaryHeap<E extends Comparable<E>> implements PriorityQueue<E> {
     private int currentSize;
 
     // The heap array.
-    protected final ArrayList<E> array;
+    protected final ArrayList<E> array; // hmap = dictionnaire en python 
+    
+    protected HashMap<E,Integer> indexMap = new HashMap<E,Integer>(); 
 
     /**
      * Construct a new empty binary heap.
@@ -49,6 +52,7 @@ public class BinaryHeap<E extends Comparable<E>> implements PriorityQueue<E> {
         }
         else {
             this.array.set(index, value);
+            this.indexMap.put(value, index); 
         }
     }
 
@@ -139,7 +143,7 @@ public class BinaryHeap<E extends Comparable<E>> implements PriorityQueue<E> {
     public void remove(E x) throws ElementNotFoundException {
         // find x
     	// TODO : find a more efficient way than indexOf
-    	int index = this.array.indexOf(x);
+    	int index = this.indexMap.get(x);
 
 
     	if (index == -1 || this.currentSize -1 < index) {
